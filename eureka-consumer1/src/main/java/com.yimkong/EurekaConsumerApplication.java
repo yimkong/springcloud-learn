3631abe0d73b423c81c7b6d2585b0384ad11399b
yimkong <17665448577@163.com>
2020-02-21 15:13:16 +0800

    bug fix

--- eureka-consumer1/src/main/java/com.yimkong/EurekaConsumerApplication.java
+++ eureka-consumer1/src/main/java/com.yimkong/EurekaConsumerApplication.java
@@ -5,9 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
- * 服务消费者 //TODO 不能成功实现向注册中心调用接口
+ * 服务消费者
  */
 @SpringBootApplication
+@EnableDiscoveryClient
 public class EurekaConsumerApplication {
 
     public static void main(String[] args) {
--- eureka-server/src/main/java/com/yimkong/eurekaserver/EurekaServerApplication.java
+++ eureka-server/src/main/java/com/yimkong/eurekaserver/EurekaServerApplication.java
@@ -3,6 +3,10 @@ package com.yimkong.eurekaserver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @SpringBootApplication
 @EnableEurekaServer
@@ -10,6 +14,16 @@ public class EurekaServerApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EurekaServerApplication.class, args);
+    }
+
+    @Configuration
+    @EnableWebSecurity
+    public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+        }
     }
 
 }
