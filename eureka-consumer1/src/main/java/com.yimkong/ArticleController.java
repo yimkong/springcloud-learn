787e7e730a0a4e6652510954bec753cf723f369d
yimkong <17665448577@163.com>
2020-02-21 17:13:38 +0800

    增加注释

--- eureka-consumer1/src/main/java/com.yimkong/ArticleController.java
+++ eureka-consumer1/src/main/java/com.yimkong/ArticleController.java
@@ -17,16 +17,18 @@ public class ArticleController {
 
     /**
      * 直接调用服务提供方，没有用到注册中心
-     *
+     * 因为不是使用别名访问，因此不能使用自动组装的restTemplate(使用了@LoadBalanced注解)
      * @return
      */
     @GetMapping("/article/toHello")
     public String hello() {
+        RestTemplate restTemplate = new RestTemplate();
         return restTemplate.getForObject("http://localhost:8081/user/hello", String.class);
     }
 
     /**
      * 直接通过注册中心调用 使用注册名spring.application.name作为路径
+     *
      * @return
      */
     @GetMapping("/article/toCenterHello")
