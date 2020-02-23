feb0b0eeaf6605f5f23ad802724a24c0420ab2ab
yimkong <17665448577@163.com>
2020-02-23 17:09:08 +0800

    eurekaClient

--- eureka-consumer1/src/main/java/com.yimkong/ArticleController.java
+++ eureka-consumer1/src/main/java/com.yimkong/ArticleController.java
@@ -1,6 +1,9 @@
 package com.yimkong;
 
+import com.netflix.discovery.EurekaClient;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -18,6 +21,7 @@ public class ArticleController {
     /**
      * 直接调用服务提供方，没有用到注册中心
      * 因为不是使用别名访问，因此不能使用自动组装的restTemplate(使用了@LoadBalanced注解)
+     *
      * @return
      */
     @GetMapping("/article/toHello")
@@ -34,6 +38,27 @@ public class ArticleController {
     @GetMapping("/article/toCenterHello")
     public String helloCenter() {
         return restTemplate.getForObject("http://eureka-client1/user/hello", String.class);
+    }
+
+    @Qualifier("eurekaClient")
+    @Autowired
+    private EurekaClient eurekaClient;
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    /**
+     * 不通过restapi 而是通过内建的api获取别的服务的信息
+     *
+     * @return
+     */
+    @GetMapping("/article/infos")
+    public Object getService1Info() {
+        return eurekaClient.getInstancesByVipAddress("eureka-client1", false);
+    }
+
+    @GetMapping("/article/infoslist")
+    public Object getService1Infos() {
+        return discoveryClient.getInstances("eureka-client1");
     }
 
 }
